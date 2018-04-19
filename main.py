48776e28b6cfe6e95bf61f9ec146871609d4a3bd
PJ Prins <pj.prins@codingwithkids.com>
2018-04-19 12:56:57 -0700

    Completed the ball class

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
 import pygame
 from pygame.locals import *
+from ball import *
 import sys
 
 pygame.init()
-
 
 size = (width, height) = (pygame.display.Info().current_w, pygame.display.Info().current_h)
 
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-image = pygame.image.load("ball.png")
-rect = image.get_rect()
-speed = [5, 5]
 color = (0, 0, 0)
-
-
-def update():
-    rect.move_ip(speed)
-
+balls = pygame.sprite.Group()
 
 
 def main():
+    global screen
+    for i in range(10):
+        balls.add(Ball((width / 2, height / 2)))
     while True:
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit()
-        update()
+            if event.type == MOUSEBUTTONDOWN:
+                balls.add(Ball(event.pos))
+            if event.type == KEYDOWN:
+                if event.key == K_d:
+                    sprites = balls.sprites()
+                    for i in range(len(balls) // 2):
+                        sprites[i].kill()
+                elif event.key == K_f:
+                    screen = pygame.display.set_mode(size, FULLSCREEN)
+                elif event.key == K_ESCAPE:
+                    screen = pygame.display.set_mode(size)
+        balls.update()
         screen.fill(color)
-        screen.blit(image, rect)
+        balls.draw(screen)
         pygame.display.flip()
 
 
-if __name__ == "__main___":
+if __name__ == "__main__":
     main()
